392752d55349746945d51cad118453d0bcffc9e2
Fortyseven <fortyseven@gmail.com>
2014-10-14 12:59:51 -0400

    Rearranged things; simplified buttons

---
+++ Assets/Demo/Head.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Head : MonoBehaviour
+{
+    public AudioClip[] Clips;
+
+    private TalkTime _talktime;
+
+    void Start()
+    {
+        _talktime = GetComponent<TalkTime>();
+    }
+
+    public void OnClip(int id)
+    {
+        _talktime.Play( Clips[ id ] );
+    }
+}
